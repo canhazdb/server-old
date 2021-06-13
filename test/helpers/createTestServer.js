7f0a7d3b46438ed96ba77f29fd03054ce94da364
Mark <me@markwylde.com>
2021-06-13 20:42:13 +1000

    Rewrite base for high availability

---
+++ test/helpers/createTestServer.js
@@ -0,0 +1,50 @@
+import fs from 'fs';
+import { v4 as uuid } from 'uuid';
+import canhazdb from '../../lib/index.js';
+
+const tls = {
+  key: fs.readFileSync('./certs/localhost.privkey.pem'),
+  cert: fs.readFileSync('./certs/localhost.cert.pem'),
+  ca: [fs.readFileSync('./certs/ca.cert.pem')],
+  requestCert: true
+};
+
+let lastUsedPort = 11000;
+const getNewPort = () => {
+  lastUsedPort = lastUsedPort + 1;
+  return lastUsedPort;
+};
+
+async function createTestServers (count) {
+  const join = [];
+
+  const servers = await Promise.all(
+    Array(count)
+      .fill(null)
+      .map(async (_, index) => {
+        const port = getNewPort();
+        const nodeName = uuid();
+
+        const server = canhazdb({
+          dataDirectory: './canhazdata/' + nodeName,
+          nodeName: nodeName,
+          host: 'localhost',
+          port: port,
+          join,
+          tls
+        });
+
+        join.push('localhost:' + port);
+
+        return server;
+      })
+  );
+
+  servers.close = function () {
+    return Promise.all(servers.map(server => server.close()));
+  };
+
+  return servers;
+}
+
+export default createTestServers;
--- utils/validateQueryOptions.js
+++
@@ -1,9 +0,0 @@
-function validateQueryOptions (options) {
-  Object.keys(options).forEach(key => {
-    if (options[key] === undefined) {
-      throw new Error('canhazdb:client can not serialise an object with undefined');
-    }
-  });
-}
-
-module.exports = validateQueryOptions;
