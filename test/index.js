26246d99a3c9c4d033df6c9c21f1653ec252e6c6
Mark Wylde <me@markwylde.com>
2021-08-22 20:58:05 +1000

    Update get logic

--- test/index.js
+++ test/index.js
@@ -9,12 +9,13 @@ basictap.on('finish', () => {
   }, 500);
 });
 
+import('./lib/driver/index.js');
+import('./lib/utils/calculateAllowedErrorCount.js');
+import('./lib/prepareOptions.js');
+
 import('./features/basic.js');
 // import('./features/cluster.js');
 import('./features/health.js');
 import('./features/notify.js');
 import('./features/lock.js');
 import('./features/systemCollections.js');
-
-import('./lib/driver/index.js');
-import('./lib/prepareOptions.js');
